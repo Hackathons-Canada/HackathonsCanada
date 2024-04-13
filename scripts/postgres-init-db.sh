dbd971d97a34f491553656c5f730e5dc1fc7e541
Jason Cameron <git@jasoncameron.dev>
2024-04-13 20:56:12 +0000

    setup perms

--- scripts/postgres-init-db.sh
+++ scripts/postgres-init-db.sh
@@ -7,11 +7,11 @@ echo '=== SETUP DATABASE'
 echo '--- setting up users'
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" << EOF
-CREATE USER hacker WITH PASSWORD '$HACKER_PASSWORD';
-ALTER ROLE hacker SET client_encoding TO 'utf8';
-ALTER ROLE hacker SET default_transaction_isolation TO 'read committed';
-ALTER ROLE hacker SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE hackers TO hacker;
-\c hackers
-GRANT ALL ON SCHEMA public TO hacker;
+CREATE USER '$POSTGRES_USER' WITH PASSWORD '$HACKER_PASSWORD';
+ALTER ROLE '$POSTGRES_USER' SET client_encoding TO 'utf8';
+ALTER ROLE '$POSTGRES_USER' SET default_transaction_isolation TO 'read committed';
+ALTER ROLE '$POSTGRES_USER' SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE '$POSTGRES_DB' TO '$POSTGRES_USER';
+\c '$POSTGRES_DB'
+GRANT ALL ON SCHEMA public TO '$POSTGRES_USER';
 EOF
