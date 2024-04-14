616d0915373fa99d74f8b174900e3677e7c3be78
Jason <git@jasoncameron.dev>
2024-04-13 21:38:48 -0400

    fmt

--- dischannelsaver/apps.py
+++ dischannelsaver/apps.py
@@ -12,15 +12,15 @@ client_thread: threading.Thread
 loop: asyncio.AbstractEventLoop
 
 
-
 class DisChannelSaverConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "dischannelsaver"
     label = "dischannelsaver"
-    
+
     def ready(self):
         global client_thread, loop
-        client_thread = threading.Thread(target=client.run, args=[settings.DISCORD_TOKEN])
+        client_thread = threading.Thread(
+            target=client.run, args=[settings.DISCORD_TOKEN]
+        )
         client_thread.start()
         loop = asyncio.get_event_loop()
-        
