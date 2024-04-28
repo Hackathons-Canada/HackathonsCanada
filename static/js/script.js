12fad586bdd92edc0a1bf3b7203df28abe1ed153
Jamerson <jamescao800@gmail.com>
2024-04-28 11:56:55 -0400

    Changed the landing page to match figma

--- static/js/script.js
+++ static/js/script.js
@@ -22,53 +22,53 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-console.log(document.querySelector("button"));
-document.querySelector("button").addEventListener("click", function () {
-  let usalist1 = document.querySelectorAll(".United");
-  usalist1.forEach((ca) => (ca.style.display = "none"));
-  let onlinelist1 = document.querySelectorAll(".Online");
-  onlinelist1.forEach((ca) => (ca.style.display = "none"));
-  let canadalist1 = document.querySelectorAll(".Canada");
-  canadalist1.forEach((ca) => (ca.style.display = "block"));
-  let otherlist1 = document.querySelectorAll(".other");
-  otherlist1.forEach((ca) => (ca.style.display = "none"));
-});
+// console.log(document.querySelector("button"));
+// document.querySelector("button").addEventListener("click", function () {
+//   let usalist1 = document.querySelectorAll(".United");
+//   usalist1.forEach((ca) => (ca.style.display = "none"));
+//   let onlinelist1 = document.querySelectorAll(".Online");
+//   onlinelist1.forEach((ca) => (ca.style.display = "none"));
+//   let canadalist1 = document.querySelectorAll(".Canada");
+//   canadalist1.forEach((ca) => (ca.style.display = "block"));
+//   let otherlist1 = document.querySelectorAll(".other");
+//   otherlist1.forEach((ca) => (ca.style.display = "none"));
+// });
 
-document
-  .getElementById("nav-contact-tabus")
-  .addEventListener("click", function () {
-    let canadalist2 = document.querySelectorAll(".Canada");
-    canadalist2.forEach((ca) => (ca.style.display = "none"));
-    let onlinelist2 = document.querySelectorAll(".Online");
-    onlinelist2.forEach((ca) => (ca.style.display = "none"));
-    let usalist2 = document.querySelectorAll(".United");
-    usalist2.forEach((ca) => (ca.style.display = "block"));
-    let otherlist1 = document.querySelectorAll(".other");
-    otherlist1.forEach((ca) => (ca.style.display = "none"));
-  });
+// document
+//   .getElementById("nav-contact-tabus")
+//   .addEventListener("click", function () {
+//     let canadalist2 = document.querySelectorAll(".Canada");
+//     canadalist2.forEach((ca) => (ca.style.display = "none"));
+//     let onlinelist2 = document.querySelectorAll(".Online");
+//     onlinelist2.forEach((ca) => (ca.style.display = "none"));
+//     let usalist2 = document.querySelectorAll(".United");
+//     usalist2.forEach((ca) => (ca.style.display = "block"));
+//     let otherlist1 = document.querySelectorAll(".other");
+//     otherlist1.forEach((ca) => (ca.style.display = "none"));
+//   });
 
-document
-  .getElementById("nav-contact-tabon")
-  .addEventListener("click", function () {
-    let canadalist = document.querySelectorAll(".Canada");
-    canadalist.forEach((ca) => (ca.style.display = "none"));
-    let usalist = document.querySelectorAll(".United");
-    usalist.forEach((ca) => (ca.style.display = "none"));
-    let onlinelist = document.querySelectorAll(".Online");
-    onlinelist.forEach((ca) => (ca.style.display = "block"));
-    let otherlist1 = document.querySelectorAll(".other");
-    otherlist1.forEach((ca) => (ca.style.display = "none"));
-  });
+// document
+//   .getElementById("nav-contact-tabon")
+//   .addEventListener("click", function () {
+//     let canadalist = document.querySelectorAll(".Canada");
+//     canadalist.forEach((ca) => (ca.style.display = "none"));
+//     let usalist = document.querySelectorAll(".United");
+//     usalist.forEach((ca) => (ca.style.display = "none"));
+//     let onlinelist = document.querySelectorAll(".Online");
+//     onlinelist.forEach((ca) => (ca.style.display = "block"));
+//     let otherlist1 = document.querySelectorAll(".other");
+//     otherlist1.forEach((ca) => (ca.style.display = "none"));
+//   });
 
-document
-  .getElementById("nav-contact-tabev")
-  .addEventListener("click", function () {
-    let canadalist = document.querySelectorAll(".Canada");
-    canadalist.forEach((ca) => (ca.style.display = "block"));
-    let usalist = document.querySelectorAll(".United");
-    usalist.forEach((ca) => (ca.style.display = "block"));
-    let onlinelist = document.querySelectorAll(".Online");
-    onlinelist.forEach((ca) => (ca.style.display = "block"));
-    let otherlist1 = document.querySelectorAll(".other");
-    otherlist1.forEach((ca) => (ca.style.display = "block"));
-  });
+// document
+//   .getElementById("nav-contact-tabev")
+//   .addEventListener("click", function () {
+//     let canadalist = document.querySelectorAll(".Canada");
+//     canadalist.forEach((ca) => (ca.style.display = "block"));
+//     let usalist = document.querySelectorAll(".United");
+//     usalist.forEach((ca) => (ca.style.display = "block"));
+//     let onlinelist = document.querySelectorAll(".Online");
+//     onlinelist.forEach((ca) => (ca.style.display = "block"));
+//     let otherlist1 = document.querySelectorAll(".other");
+//     otherlist1.forEach((ca) => (ca.style.display = "block"));
+//   });
