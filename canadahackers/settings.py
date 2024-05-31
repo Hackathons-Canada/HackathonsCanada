870f1bb6bb29abffce817f3d17506fe8ba394548
Jason <git@jasoncameron.dev>
2024-05-30 22:10:54 -0400

    Auth backends

--- canadahackers/settings.py
+++ canadahackers/settings.py
@@ -78,6 +78,13 @@ TEMPLATES = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 WSGI_APPLICATION = "canadahackers.wsgi.application"
 
 # Database
