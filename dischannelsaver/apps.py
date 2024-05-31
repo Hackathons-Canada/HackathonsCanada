d5d7f76ad674358e19ff74f28598d42e329fbb39
Jason <git@jasoncameron.dev>
2024-05-30 20:30:47 -0400

    Removed error

--- dischannelsaver/apps.py
+++ dischannelsaver/apps.py
@@ -12,6 +12,14 @@ client_thread: threading.Thread
 loop: asyncio.AbstractEventLoop
 
 
+def run(token: str):
+    try:
+        client.run(token)
+    except discord.errors.LoginFailure:
+        print("Note: Discord bot token is invalid")
+        pass
+    
+
 class DisChannelSaverConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "dischannelsaver"
@@ -20,7 +28,7 @@ class DisChannelSaverConfig(AppConfig):
     def ready(self):
         global client_thread, loop
         client_thread = threading.Thread(
-            target=client.run, args=[settings.DISCORD_TOKEN]
+            target=run, args=[settings.DISCORD_TOKEN]
         )
         client_thread.start()
         loop = asyncio.get_event_loop()
