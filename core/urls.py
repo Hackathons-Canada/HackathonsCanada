8c0684ba5ce18bc4f79d44f86f08fb7d7b0b9dc7
nirek13 <nirekshetty@gmail.com>
2024-07-02 20:44:49 -0400

    calendar type shit

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("hackathons/", views.HackathonsPage.as_view(), name="hackathons"),
-    path('add-hackathons/', views.addHackathons, name='add_hackathons')
+    path('add-hackathons/', views.addHackathons, name='add_hackathons'),
+path('calendar/', views.calendar(), name='calendar')
 ]
--- core/views.py
+++ core/views.py
@@ -16,9 +16,18 @@ def addHackathons(request):
     }
     return render(request, '../templates/add_hackathon.html', context)
 
+
+def calendar(request):
+    context = {
+        'title': 'Add a New Hackathon',
+        'content': 'Use this form to add a new hackathon to the database.'
+    }
+    return render(request, '../templates/calendar.html', context)
+
+
 class HackathonsPage(ListView):
     template_name = "hackathons.html"
     context_object_name = "hackathons"
+
     def get_queryset(self):
         return Hackathon.objects.all()
-
