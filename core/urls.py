b67795126fc8e5e3ba4b01a4d54caad87c90e80e
nirek13 <nirekshetty@gmail.com>
2024-07-02 20:45:20 -0400

    calendar type shit

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("hackathons/", views.HackathonsPage.as_view(), name="hackathons"),
     path('add-hackathons/', views.addHackathons, name='add_hackathons'),
-path('calendar/', views.calendar(), name='calendar')
+path('calendar/', views.calendar, name='calendar')
 ]
