609b3006fed2d52302da75ff08bab7a1591dee80
Jason <git@jasoncameron.dev>
2024-07-14 20:22:26 -0400

    redid init script

--- scripts/postgres-init-db.sh
+++ scripts/postgres-init-db.sh
@@ -4,14 +4,22 @@ set -eu
 
 echo '=== SETUP DATABASE'
 
+# --- Create the database only if it doesn't exist
+echo '--- creating database (if not exists)'
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+SELECT 'CREATE DATABASE $POSTGRES_DB'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$POSTGRES_DB')\gexec
+EOSQL
+
 echo '--- setting up users'
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" << EOF
-CREATE USER '$POSTGRES_USER' WITH PASSWORD '$POSTGRES_PASSWORD';
-ALTER ROLE '$POSTGRES_USER' SET client_encoding TO 'utf8';
-ALTER ROLE '$POSTGRES_USER' SET default_transaction_isolation TO 'read committed';
-ALTER ROLE '$POSTGRES_USER' SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE '$POSTGRES_DB' TO '$POSTGRES_USER';
-\c '$POSTGRES_DB'
-GRANT ALL ON SCHEMA public TO '$POSTGRES_USER';
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOF
+CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';
+ALTER ROLE $POSTGRES_USER SET client_encoding TO 'utf8';
+ALTER ROLE $POSTGRES_USER SET default_transaction_isolation TO 'read committed';
+ALTER ROLE $POSTGRES_USER SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+\c $POSTGRES_DB
+GRANT ALL ON SCHEMA public TO $POSTGRES_USER';
 EOF
