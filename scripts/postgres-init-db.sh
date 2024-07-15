0e497fc53d0eaa7f8e4ce98ae7613cc815377a5c
Jason <git@jasoncameron.dev>
2024-07-14 21:15:00 -0400

    try a db redo

--- scripts/postgres-init-db.sh
+++ scripts/postgres-init-db.sh
@@ -4,11 +4,16 @@ set -eu
 
 echo '=== SETUP DATABASE'
 
-echo '--- setting up user'
+echo '--- setting up users'
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOF
-ALTER ROLE $POSTGRES_USER SET client_encoding TO 'utf8';
-ALTER ROLE $POSTGRES_USER SET default_transaction_isolation TO 'read committed';
-ALTER ROLE $POSTGRES_USER SET timezone TO 'UTC';
-
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" << EOF
+CREATE USER hacker WITH PASSWORD 'change_me_pleaseeeeeeeeee';
+ALTER ROLE hacker SET client_encoding TO 'utf8';
+ALTER ROLE hacker SET default_transaction_isolation TO 'read committed';
+ALTER ROLE hacker SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE hackathons TO hacker;
+\c hackathons
+GRANT ALL ON SCHEMA public TO hacker;
 EOF
+
+cat ./pg_dump.sql.gz | gzip -d | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" # update pg_dump.sql.gz
