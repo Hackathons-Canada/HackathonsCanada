fdaa71361b018c6dbd5fb62635635e6218093e49
CHIRAG DALMIA <119760552+ChiragDalmia@users.noreply.github.com>
2024-07-19 19:50:32 -0400

    moved calender initialization to a seperate file

--- static/js/script.js
+++ static/js/script.js
@@ -8,13 +8,3 @@
 //     });
 //   });
 // });
-
-document.addEventListener("DOMContentLoaded", function () {
-  var calendarEl = document.getElementById("calendar");
-  if (calendarEl) {
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: "dayGridMonth",
-    });
-    calendar.render();
-  }
-});
