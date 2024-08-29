1e8425098311df63b35708d780f077c9ea729ea1
Jason <git@jasoncameron.dev>
2024-08-28 21:56:55 -0400

    added tailwind dirs

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "templates/**/*.html",  // Adjust if your app name is different
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
