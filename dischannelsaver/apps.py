bee03882a019f5e329f6f8d2c92d900f7a60e430
Jason <git@jasoncameron.dev>
2024-08-28 23:21:47 -0400

    only initalize discord if token is provided

--- dischannelsaver/apps.py
+++ dischannelsaver/apps.py
@@ -4,6 +4,7 @@ import threading
 import discord
 from django.apps import AppConfig
 from django.conf import settings
+import re
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -26,6 +27,13 @@ class DisChannelSaverConfig(AppConfig):
     label = "dischannelsaver"
 
     def ready(self):
+        if settings.DISCORD_TOKEN is None or not re.match(
+            r"^([MN][\w-]{23,25})\.([\w-]{6})\.([\w-]{27,39})$", settings.DISCORD_TOKEN
+        ):
+            print(
+                "Note: DISCORD_TOKEN is not set in settings.py or is invalid, if you want to use the discord bot, please set it"
+            )
+            return
         global client_thread, loop
         client_thread = threading.Thread(target=run, args=[settings.DISCORD_TOKEN])
         client_thread.start()
--- hackathons_canada/settings.py
+++ hackathons_canada/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
     "localhost",
     # Add any other allowed hosts here
 ]
+INTERNAL_IPS = ["127.0.0.1"]
 
 AUTH_USER_MODEL = "core.Hacker"
 # Application definition
