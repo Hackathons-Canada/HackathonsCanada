a4c88f985dea20fa1f3439862e209d3f9741b1bb
joutad <joudat01@gmail.com>
2024-10-25 17:26:57 -0400

    WIP initial commit

--- hackathons_canada/celery.py
+++ hackathons_canada/celery.py
@@ -1,6 +1,9 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
+
+# from core.models import Hacker
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackathons_canada.settings")
@@ -12,6 +15,14 @@ app = Celery("hackathons_canada")
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.beat_schedule = {
+    "send_new_hackathon_email": {
+        "task": "core.tasks.send_new_hackathon_email",
+        "schedule": crontab(),
+        # 'args': Hacker
+    }
+}
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
