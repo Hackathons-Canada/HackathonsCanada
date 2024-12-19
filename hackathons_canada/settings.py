048e17c0e980e7a3116f009efeeed7e64df2548e
Jason Cameron <git@jasoncameron.dev>
2024-12-19 01:35:46 -0500

    Add local compose file with watch support

--- hackathons_canada/settings.py
+++ hackathons_canada/settings.py
@@ -21,6 +21,8 @@ from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(BASE_DIR)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
