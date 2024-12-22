bac9deff820a36d5d1193ba9e376424df49b4813
James Cao <jameslodgemath@gmail.com>
2024-12-21 21:43:59 -0500

    undid and added all the password stuff

--- hackathons_canada/settings.py
+++ hackathons_canada/settings.py
@@ -165,8 +165,16 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
-ACCOUNT_PASSWORD_MIN_LENGTH = 1  # Set minimum length to 1 or your desired length
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
