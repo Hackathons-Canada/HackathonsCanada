eb65f5fdd62f65e6f7a0a3b119c12a08e6b17136
Jason Cameron <git@jasoncameron.dev>
2024-12-25 17:38:36 -0500

    feat: add debug toolbar

--- settings/environments/development.py
+++ settings/environments/development.py
@@ -15,11 +15,21 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "[::1]",
 ]
+
+INTERNAL_IPS = ["127.0.0.1"]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
-MIDDLEWARE.insert(0, "silk.middleware.SilkyMiddleware")
-INSTALLED_APPS.append("silk")
-SILKY_PYTHON_PROFILER = True
+
+# SILK
+# MIDDLEWARE.insert(0, "silk.middleware.SilkyMiddleware")
+# INSTALLED_APPS.append("silk")
+# SILKY_PYTHON_PROFILER = True
+
+
+# DEBUG TOOLBAR
+INSTALLED_APPS.append("debug_toolbar")
+MIDDLEWARE.insert(1, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
 # Disable persistent DB connections
 # https://docs.djangoproject.com/en/4.2/ref/databases/#caveats
@@ -29,3 +39,15 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 STATIC_ROOT = "/static"
+
+
+# Needed if you want to use the map widgets
+# MAP_WIDGETS["GoogleMap"]["apiKey"] = "CHANGE_ME"  # noqa
+
+# If you want to override the default logging location
+# LOGGING["handlers"]["file"]["filename"] = "/logs/access.log"  # noqa
+
+DISCORD_GUILD_ID = 1186761885354311780
+DISCORD_ARCHIVE_CATEGORY_ID = 1196919673363632178
+DISCORD_ACTIVE_CATEGORY_ID = 1186762053180993576
+DISCORD_TOKEN = ""
