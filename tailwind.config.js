11506fcea36330194f7c749b110a97ef1c6565bd
James Cao <jameslodgemath@gmail.com>
2025-01-08 16:55:58 -0500

    chagned the tailwind config so the bg images render on prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "templates/**/*.html"
+        "templates/**/*.html",
+        "./static/**/*"
     ],
     theme: {
         extend: {},
